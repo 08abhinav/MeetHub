a64eb1d5742a212ab1564ec04fa3847fc23265a0
08abhinav <abhinavnegi101@gmail.com>
2025-05-12 14:21:52 +0530

    feat: created meeting features and acitons

---
+++ src/app/components/MeetingAction.tsx
@@ -0,0 +1,132 @@
+"use client"  
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { v4 as uuidv4 } from 'uuid';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
+import { Copy, Link2, LinkIcon, Loader2, Plus, Video } from 'lucide-react';
+import { useSession } from 'next-auth/react'
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog';
+
+export const MeetingAction = () => {
+  const {data: session} = useSession();
+  const [isLoading, setIsLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [baseUrl, setBaseUrl] = useState('');
+  const [generatedUrl, setGeneratedUrl] = useState('');
+  const [meetingLink, setMeetingLink] = useState('');
+  const router = useRouter();
+
+  useEffect(()=>{
+    setBaseUrl(window.location.origin);
+  }, [])
+
+  const handleMeeting = ()=>{
+    const roomId = uuidv4();
+    const url = `${baseUrl}/video-meeting/${roomId}`;
+    setGeneratedUrl(url);
+    setOpen(true);
+  }
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(generatedUrl);
+    toast.info("Copied to clipboard")
+  }
+
+  const handleJoinMeeting = async () => {
+    if(meetingLink){
+      setIsLoading(true);
+      const formatLink =  meetingLink.includes('http') ? meetingLink : `${baseUrl}/${meetingLink}`;
+      router.push(formatLink)
+      toast.info('joining meeting')
+    }else{
+      toast.error("please enter a valid link")
+    }
+  };
+
+  const handleInstantMeeting = async () => {
+    setIsLoading(true);
+    const roomId = uuidv4();
+    const url = `${baseUrl}/video-meeting/${roomId}`;
+    setTimeout(() => {
+      router.push(url);
+    }, 100);
+    // toast.info("creating meeting")
+  }
+
+  return (
+    <>
+      <div className='flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4'>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              className='w-full sm:w-auto text-[#DFD0B8] bg-gradient-to-r from-[#464a50] to-[#5a5c5e] 
+              shadow-lg cursor-pointer hover:shadow-xl focus:!bg-transparent active:!bg-transparent'
+              size="lg">
+              <Video className='w-5 h-5 mr-2' />
+              New Meeting
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent className='bg-gradient-to-r from-[#464a50] to-[#5a5c5e] border text-[#DFD0B8] w-[250px] ml-6 rounded-lg'>
+            {/* Meeting for later */}
+            <DropdownMenuItem onClick={handleMeeting} 
+            className='text-[#DFD0B8] hover:bg-[#5a5c5e] cursor-pointer flex items-center p-4 rounded-lg'>
+              <Link2 className='w-4 h-4 mr-2' />
+              Create a meeting for later
+            </DropdownMenuItem>
+
+            {/* Instant meeting */}
+            <DropdownMenuItem onClick={handleInstantMeeting}
+            className='text-[#DFD0B8] hover:bg-[#5a5c5e] cursor-pointer flex items-center p-4 rounded-lg'>
+              <Plus className='w-4 h-4 mr-2'/>
+              Start an instant meeting
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        {/* meeting code */}
+        <div className='flex w-full sm:w-auto relative'>
+          <span className='absolute left-2 top-1/2 transform -translate-y-1/2'>
+            <LinkIcon className='w-4 h-4 text-[#DFD0B8]'/>
+          </span>
+          <Input 
+          className='pl-8 shadow-lg text-[#DFD0B8] rounded-r-none pr-10 bg-gradient-to-r from-[#464a50] to-[#5a5c5e] '
+          placeholder='Enter a code or link'
+          value={meetingLink}
+          onChange={(e)=>{setMeetingLink(e.target.value)}}/>
+        <Button
+          variant="secondary"
+          className="rounded-l-none text-[#DFD0B8] ml-1 bg-[#5a5c5e] cursor-pointer hover:bg-[#595a5b] focus:bg-[#595a5b] active:bg-[#595a5b] shadow-lg"
+          onClick={handleJoinMeeting}>
+          Join
+        </Button>
+        </div>
+
+      </div>
+      {/* dialog box */}
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="rounded-3xl bg-gradient-to-r from-[#464a50] to-[#5a5c5e] border border-[#6b6c6f] text-[#DFD0B8] w-[400px]">
+          <DialogTitle className="text-3xl font-normal text-[#DFD0B8] text-center mb-4">
+              Here's your joining information
+          </DialogTitle>
+          <div className="flex flex-col items-center justify-center">
+            <span className="text-[#DFD0B8] font-semibold text-lg mb-2">
+              {generatedUrl.slice(0, 30)}...
+            </span>
+            <Button
+              variant="ghost"
+              className="bg-[#5a5c5e] text-[#DFD0B8] hover:bg-[#595a5b] focus:bg-[#595a5b] active:bg-[#595a5b] shadow-lg cursor-pointer"
+              onClick={copyToClipboard}
+            >
+              <Copy className="w-5 h-5" />
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+
+    </>
+  )
+}
---
+++ src/app/components/MeetingFeature.tsx
@@ -0,0 +1,82 @@
+import { Button } from '@/components/ui/button'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+import Image from 'next/image'
+import React, { useState } from 'react'
+
+const slides = [
+  {
+    image: "https://www.gstatic.com/meet/user_edu_get_a_link_light_90698cd7b4ca04d3005c962a3756c42d.svg",
+    title: "Get a link you can share",
+    description: "Click 'New Meeting' to schedule a session in TalkDest and send invites to participants",  },
+  {
+    image: "https://www.gstatic.com/meet/user_edu_scheduling_light_b352efa017e4f8f1ffda43e847820322.svg",
+    title: "Plan ahead",
+    description: "Click 'New Meeting' to get a shareable link for instant TalkDest sessions",  },
+  {
+    image: "https://www.gstatic.com/meet/user_edu_safety_light_e04a2bbb449524ef7e49ea36d5f25b65.svg",
+    title: "Your meeting is safe",
+    description: "Only invited participants or those admitted by the host can join a TalkDest meeting",  },
+]
+
+const MeetingFeature = () => {
+  const [currentSlide, setCurrentSlide] = useState(0)
+  
+  const handleNextSlide = () => {
+    const nextSlide = (currentSlide + 1) % slides.length
+    setCurrentSlide(nextSlide)
+  }
+
+  const handlePrevSlide = () => {
+    const prevSlide = (currentSlide - 1 + slides.length) % slides.length
+    setCurrentSlide(prevSlide)
+  }
+
+
+  return (
+    <div className='flex flex-col items-center'>
+      <div className='relative'>
+        <Image
+          src={slides[currentSlide].image}
+          alt="meeting-image" 
+          width={400}
+          height={400}
+          className='rounded-full w-40 h-40 md:w-64 md:h-64'
+        />
+    
+        {/* Previous Button */}
+        <Button
+          variant="ghost"
+          size="icon"
+          className='text-[#DFD0B8] cursor-pointer absolute top-1/2 -left-10 transform -translate-y-1/2'
+          onClick={handlePrevSlide}>
+          <ChevronLeft className='h-6 w-6'/>
+        </Button>
+    
+        {/* Next Button */}
+        <Button
+          variant="ghost"
+          size="icon"
+          className='text-[#DFD0B8] cursor-pointer absolute top-1/2 -right-10 transform -translate-y-1/2'
+          onClick={handleNextSlide}>
+          <ChevronRight className='h-6 w-6'/>
+        </Button>
+      </div>
+      <h2 className='text-2xl text-[#DFD0B8] font-semibold text-center mt-6 mb-2'>
+          {slides[currentSlide].title}
+      </h2>
+      <p className='text-[#948979] text-lg text-center text max-w-sm'>
+          {slides[currentSlide].description}
+      </p>
+      <div className='flex justify-center space-x-2 mt-2'>
+        {slides.map((_, index)=> (
+          <div key={index}
+          className={`w-3 h-3 rounded-full ${index === currentSlide ? "bg-[#948979]" : "bg-gray-700"}`}>
+          </div>
+        ))}
+      </div>
+    </div>
+  
+  )
+}
+
+export default MeetingFeature
