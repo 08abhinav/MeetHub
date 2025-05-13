e0de640e7f5fae2b6e4cf2441e24c1e8eb5a12b5
08abhinav <abhinavnegi101@gmail.com>
2025-05-13 18:02:11 +0530

    feat: separated signup form for user

--- src/app/newUser/page.tsx
+++ src/app/newUser/page.tsx
@@ -1,26 +1,64 @@
 "use client"
-import React, { useState } from 'react'
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React, { FormEvent, useState } from 'react'
+import { toast } from 'react-toastify';
+import Loader from '../components/Loader';
 
 const page = () => {
+    const route = useRouter();
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [profilePicture, setProfilePicture] = useState<File | null>(null);
+    const [isLoading, setIsLoading] = useState(false)
 
-    const handleSubmition = (e:any)=>{
+    const handleSubmition = async (e:FormEvent)=>{
         e.preventDefault();
+        setIsLoading(true);
 
-        setName('')
-        setEmail('')
-        setPassword('')
+        try{
+            const res = await fetch('/api/signup', {
+                method: "POST",
+                headers:{
+                    'Content-Type': "application/json"
+                },
+                body:JSON.stringify({
+                    fname: name,
+                    email,
+                    password,
+                    profilePicture: '',
+                })
+            })
+
+           const data = await res.json();
+           if(!res.ok){
+                toast.error("something went wrong");
+                console.error("signup error: ", data.message)
+           }
+            
+           toast.success("signup successful")
+           route.push('/')
+
+        }catch(error){
+            console.error("signup error: ", error);
+            toast.error("something went wrong")
+        }
+        setName('');
+        setEmail('');
+        setPassword('');
+        setProfilePicture(null);
     }
+
   return (
     <div className='flex justify-center bg-[#393E46] h-screen w-full'>
         <div className='flex flex-col justify-center items-center bg-gradient-to-r from-[#383c41] to-[#464a50] 
         h-auto w-[90%] md:w-1/2 m-4 border-2 border-[#464a50] rounded-2xl shadow-lg p-8'>
             <div className='text-[#DFD0B8] font-bold text-3xl mb-6'>
-                Welcome to MeetHub
+                Create your MeetHub account
             </div>
-
+            {isLoading && <Loader/>}
             <form className='w-full space-y-4' onSubmit={handleSubmition}>
             {/* Name Field */}
             <div className='flex flex-col'>
@@ -30,7 +68,7 @@ const page = () => {
                 value={name}
                 onChange={(e)=> {setName(e.target.value)}}
                 id='name'
-                name='name'
+                name='fname'
                 placeholder='Enter your name'
                 className='rounded-md px-4 py-2 bg-[#2E3238] text-white border border-[#5c6067] 
                 focus:outline-none focus:ring-2'
@@ -78,10 +116,14 @@ const page = () => {
                 id='profilePicture'
                 name='profilePicture'
                 accept='image/*'
-                className='file:rounded-md file:px-4 file:py-2 file:hover:border-2 border-white hover:cursor-pointer file:border-0 file:font-semibold
+                className='rounded-md file:px-4 file:py-2 bg-[#2E3238] border-2 border-[#5c6067] hover:cursor-pointer file:border-0 file:font-semibold
                 text-white mt-2'
-                required
                 />
+            </div>
+
+            {/* home redirect */}
+            <div className='flex flex-col'>
+                <p className='text-blue-400'>Already have an account? <Link href="/user-auth" className='text-blue-400 hover:underline hover:text-blue-500 cursor-pointer'>Login</Link></p>
             </div>
 
             {/* Submit Button */}
