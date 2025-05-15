3e6f76c542072f87b90a59871ba9571e1d8ae916
08abhinav <abhinavnegi101@gmail.com>
2025-05-15 20:53:25 +0530

    update: changed the header style

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,17 +2,19 @@
 import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
-import { Info, LogOut, Plus, Video, X } from 'lucide-react';
+import { Info, LogOut, Plus, Router, Video, X } from 'lucide-react';
 import { signOut, useSession } from 'next-auth/react';
-import { useTheme } from 'next-themes';
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
+import Loader from './Loader';
+import { useRouter } from 'next/navigation';
 
 const Header = () => {
-  const { theme, setTheme } = useTheme();
   const { data: session, status } = useSession();
   const [open, setOpen] = useState(false); 
+  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
 
   const formatTimeDate = ()=>{
     const currDate = new Date();
@@ -26,22 +28,36 @@ const Header = () => {
     })
   }
 
-  const userPlaceHolder = session?.user?.name?.charAt(0).toUpperCase() || "U";
+  const userPlaceHolder = session?.user?.name?.charAt(0).toUpperCase() || "Sign-in";
+  // console.log("Name: ", session?.user)
   const handleLogout:any = async () => {
     await signOut({ callbackUrl: '/user-auth' });
     toast.success("Logged out successfully")
   }
 
+  const handleSignin = async()=>{
+    setIsLoading(true);
+    setTimeout(()=>{
+      router.push('/user-auth')
+    })
+  }
   return (
-    <div className="flex items-center justify-between shadow-[0_10px_20px_-10px_rgba(90,90,90,0.6)] z-50
-     bg-[#464a50] p-6 ">
+    <div className="flex items-center justify-between shadow-[0_10px_20px_-10px_rgba(255,200,150,0.6)] z-50
+     bg-black p-6 ">
       {/* Talkdesk logo */}
       <div className="flex items-center space-x-4">
         <Link href="/" className='flex items-center space-x-2'>
             <Video className="w-8 h-8 text-[#DFD0B8]" />
-            <span className="hidden md:block font-semibold text-3xl text-[#DFD0B8]">
-              MeetHub
-            </span>
+            <div className='flex flex-col ml-2'>
+              <span className="hidden md:block font-semibold text-3xl text-[#DFD0B8]">
+                MeetHub
+              </span>
+              <span className='text-sm font-semibold text-[#DFD0B8]'>
+                Connect.
+                Collaborate.
+                Communicate.
+              </span>
+            </div>
         </Link>
       </div>
 
@@ -57,6 +73,8 @@ const Header = () => {
           <Info className='w-5 h-5 ml-2 text-[#DFD0B8]'/>
         </Button>
 
+        {/* If user is not logged in */}
+        {session?.user?(
         <DropdownMenu open={open} onOpenChange={setOpen}>
           <DropdownMenuTrigger asChild>
             {/* user image */}
@@ -78,12 +96,14 @@ const Header = () => {
           <DropdownMenuContent align="end" className='w-80 p-4 bg-[#464a50] rounded-xl shadow-lg border z-[999]'>
             {/* user email */}
             <div className='flex justify-between items-center mb-4'>
-                <span className='text-sm font-bold text-[#DFD0B8]'>
-                  {session?.user?.email}
-                </span>
-                <Button variant="ghost" size='icon' onClick={()=>setOpen(false)} className='rounded-full p-4 focus:!bg-transparent active:!bg-transparent data-[state=open]:!bg-transparent hover:cursor-pointer'>
-                  <X className='h-4 w-5 text-[#DFD0B8]' />
-                </Button>
+              <span className='text-sm font-bold text-[#DFD0B8]'>
+                {session?.user?.email}
+              </span>
+              
+              {/* cancle button */}
+              <Button variant="ghost" size='icon' onClick={()=>setOpen(false)} className='rounded-full p-4 focus:!bg-transparent active:!bg-transparent data-[state=open]:!bg-transparent hover:cursor-pointer'>
+                <X className='h-4 w-5 text-[#DFD0B8]' />
+              </Button>
             </div>
             
             {/* user profile inside drop-down menu */}
@@ -118,7 +138,13 @@ const Header = () => {
               </Button>
             </div>
           </DropdownMenuContent>
-        </DropdownMenu>
+        </DropdownMenu>):(
+          <Button
+          onClick={handleSignin}
+          className="text-[#DFD0B8] border border-[#DFD0B8] hover:cursor-pointer shadow-lg" variant="outline">
+           {isLoading ? <Loader/> : "Sign-In"}
+          </Button>
+        )}
       </div>
     </div>
   );
