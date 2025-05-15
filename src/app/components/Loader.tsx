0943fc1bac2ea9ced03e8c92115cf1d02305f8b7
08abhinav <abhinavnegi101@gmail.com>
2025-05-15 21:43:02 +0530

    update: style and alignment updation

--- src/app/components/Loader.tsx
+++ src/app/components/Loader.tsx
@@ -3,8 +3,8 @@ import { Loader2 } from 'lucide-react'
 
 const Loader = () => {
   return (
-    <div className='flex fixed inset-0  items-center justify-center bg-black bg-opacity-50 z-50'>
-        <Loader2 className='w-8 h-8 text-white animate-spin'/>
+    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/20">
+      <Loader2 className="w-10 h-10 text-white animate-spin duration-700" />
     </div>
   )
 }
