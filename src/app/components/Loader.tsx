949de31b1b215a9ca92e2983e8fd4eaf66a66e89
08abhinav <abhinavnegi101@gmail.com>
2025-05-27 15:17:56 +0530

    update: changed style

--- src/app/components/Loader.tsx
+++ src/app/components/Loader.tsx
@@ -4,7 +4,7 @@ import { Loader2 } from 'lucide-react'
 const Loader = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/20">
-      <Loader2 className="w-10 h-10 text-white animate-spin duration-700" />
+      <Loader2 className="w-12 h-12 text-white animate-spin duration-700" />
     </div>
   )
 }
